20dc6418ab91ec0bbbd7117ef0e2c1217ed4a824
Aurelia Carlin <aricarlin@gmail.com>
2019-01-22 11:12:12 -0500

    Getting notes from specific users

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -22,7 +22,7 @@ namespace ElevenNote.Data
         public string Content { get; set; }
 
         [Required]
-        public DateTimeOffset CreateUtc { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -12,7 +12,7 @@ namespace ElevenNote.Models
         public int NoteId { get; set; }
         public string Title { get; set; }
 
-        [Display(Name="Created")]
+        [Display(Name ="Created")]
         public DateTimeOffset CreatedUtc { get; set; }
 
         public override string ToString() => Title;
