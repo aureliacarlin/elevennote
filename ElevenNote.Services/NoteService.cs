34b1630cbde3cdf557cae04c40678b4dcfa1bae3
Aurelia Carlin <aricarlin@gmail.com>
2019-01-23 10:24:33 -0500

    I fixed it! I wasn't calling the argument correctly

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -11,7 +11,8 @@ namespace ElevenNote.Services
     public class NoteService
     {
         private readonly Guid _userId;
-        private int noteId;
+        
+       
 
         public NoteService(Guid userId)
         {
@@ -62,14 +63,14 @@ namespace ElevenNote.Services
                 var entity =
                     ctx
                     .Notes
-                    .Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
                 return
                     new NoteDetail
                     {
                         NoteId = entity.NoteId,
                         Title = entity.Title,
                         Content = entity.Content,
-                        CreatedUtc = entity.CreatedUtc,
+                        CreatedUtc = entity.CreateUtc,
                         ModifiedUtc = entity.ModifiedUtc
                     };
             }
